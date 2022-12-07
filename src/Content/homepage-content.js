b4818b1e823bf78fd0db58f20c518470ab820505
DawidWebb <tomasz.matras@developerweb.pl>
2022-12-07 09:40:50 +0100

    Add homepage content, global styles and  variables

---
+++ src/Content/homepage-content.js
@@ -0,0 +1,74 @@
+export const firstSection = {
+	bigTitle: "Z NAMI JESTEŚ NA DOBREJ DRODZE!",
+	title1: "Ewidencja czasu pracy kierowców – MAWEX",
+	content1:
+		"Firma MAWEX oferuje kompleksowe usługi w zakresie prowadzenia ewidencji czasu pracy kierowców. Zapewniamy wsparcie przedsiębiorcom, przejmując obowiązek wykonywania szczegółowej analizy czasu pracy kierowcy, rozliczania czasu pracy kierowcy, oceny ryzyka wystąpienia naruszeń oraz sprawdzania ciągłości zapisów aktywności kierowców. Korzyścią z podjęcia współpracy jest przede wszystkim rzetelnie przygotowana ewidencja czasu pracy kierowcy, dokładnie wyliczone delegacje krajowe i zagraniczne, nadzorowanie terminowego pobierania danych z tachografów cyfrowych i kart kierowców oraz weryfikację ciągłości zapisów aktywności kierowców co przekłada się dla przedsiębiorcy na oszczędność czasu oraz kosztów. Dodatkowo zapewniamy naszym klientom wstępny audyt dokumentacji, okresowe szkolenia kierowców a także pomoc w zarządzaniu środkami transportu.",
+	title2: "Prawo a ewidencja czasu pracy kierowcy",
+	content2:
+		"Częsta zmienność przepisów dotyczących czasu pracy kierowcy stanowi poważne utrudnienie dla przedsiębiorców. Samodzielne analizowanie wykresówek czy danych z kart kierowcy bywa czasochłonne i wiąże się z koniecznością posiadania aktualnej wiedzy na temat przepisów prawnych regulujących sposób analizy i rozliczenia czasu pracy kierowców. Brak prowadzenia ewidencji czasu pracy kierowcy lub nieprawidłowości w dokumentach mogą wiązać się z konsekwencjami w postaci kary grzywny. Klienci, którzy z nami współpracują, otrzymują profesjonalnie wykonane raporty i dokumentację, które przygotowano na podstawie aktualnie obowiązujących przepisów. Wciąż doskonalimy swoje umiejętności w celu zaspokojenia potrzeb naszych klientów. Nieustannie śledzimy zmieniające się taryfikatory, ustawy i rozporządzenia wydawane w Polsce i Unii Europejskiej.",
+	title3: "Posługujemy się najlepszymi narzędziami",
+	content3:
+		"Znajomość prawa wykorzystujemy do szkolenia kierowców. Aktualna wiedza oraz świadomość obowiązków pozwala zminimalizować ilość popełnianych naruszeń w pracy kierowców i zredukować koszty, które są z nimi związane. Działamy na najlepszym dostępnym na rynku oprogramowaniu 4Trans. Program jest rekomendowany przez Inspekcję Transportu Drogowego i Państwową Inspekcję Pracy oraz na bieżąco aktualizowany o wszelkie zmiany prawne w Polsce i UE. System umożliwia kontrolę i analizę czasu pracy kierowcy, a także weryfikację manipulacji tachografów cyfrowych. Zlecenie nam ewidencji czasu pracy kierowców zatrudnionych w Państwa firmie to doskonały sposób na oszczędności. Przedsiębiorcy nie tylko nie muszą opłacać dostępu do oprogramowania, ale również zatrudniać i szkolić pracownika lub pracowników, którzy byliby odpowiedzialni za wykonywanie czynności związanych z nadzorem nad pracą kierowców.",
+};
+
+export const offerSection = [
+	{
+		id: 1,
+		title: "Analiza czasu pracy kierowców",
+		content:
+			"Nasi specjaliści dysponują profesjonalnym i specjalistycznym sprzętem oraz niezbędną wiedzą na temat przepisów, aby dokonać precyzyjnej analizy zapisów z analogowych i cyfrowych tachografów. Samodzielna analiza, daje zwykle niepełny obraz o popełnianych wykroczeniach w czasie pracy kierowców. Analizując czas pracy możemy szybko zareagować na najczęściej popełniane wykroczenia i zminimalizować wysokość kar w przyszłości. Nasz specjalista przygotuje pełen zestaw raportów z danego okresu, zawierający przejrzysty czas pracy kierowcy, samochodu, wraz z opisem popełnionych naruszeń i wysokością możliwych kar finansowych. Analiza czasu pracy kierowców umożliwia ich kontrolę pod kątem przestrzegania Rozporządzenia 561/2006 oraz Umowy AETR.",
+	},
+	{
+		id: 2,
+		title: "Ewidencja czasu pracy kierowców",
+		content:
+			"Nasza firma precyzyjnie opracowuje pełną ewidencję czasu pracy kierowców w oparciu o zapis na wykresówkach i kartach kierowców wraz z należnym wynagrodzeniem uwzględniającym wypłacany ryczałt. Ewidencja zawiera: godziny planowanej i wykonanej pracy,nadgodziny Godziny pracy nocnej, dyżury Dni wolne",
+	},
+	{
+		id: 3,
+		title: "Ciągłość zapisów aktywności kierowców",
+		content:
+			"Podczas wykonywania analizy danych z tarcz oraz karty kierowcy sprawdzamy ciągłość aktywności kierowcy oraz wskażemy Państwu okres, w którym ciągłość ta nie jest zachowana. Wtedy niezbędne będzie posiadanie przez Państwa dokumentów potwierdzających ten stan – o czym poinformujemy.",
+	},
+	{
+		id: 4,
+		title: "Ocena ryzyka wystąpienia naruszeń",
+		content:
+			"Ocena ryzyka wystąpienia naruszeń jest to wskaźnik generowany przez Inspekcję Transportu Drogowego podczas kontroli na drodze i zapisywany w centralnej bazie danych GITD. Wielkość współczynnika ryzyka uzależniona jest od liczby i rodzaju wykroczeń kierowcy. Gdy łączna liczba punktów zbliża się do wartości granicznej, właściciel firmy transportowej w następnym roku może być wytypowany do przeprowadzenia kontroli w siedzibie przedsiębiorcy. Pułap liczby punktów decydujący o kontroli jest wartością zmienną w każdym roku. Analizując dany okres pracy kierowcy, mamy możliwość wskazania Państwu uchybień, szybkiego zareagowania na błędy i uświadomienie kierowcy, że popełnia naruszenia, które mogą negatywnie wpłynąć na funkcjonowanie przedsiębiorstwa.",
+	},
+	{
+		id: 5,
+		title: "Delegacje",
+		content: "Wyliczymy dla Państwa wysokość diety przysługującej kierowcy za jego czas pracy poza siedzibą firmy.",
+	},
+	{
+		id: 6,
+		title: "Pobieranie danych z tachografów cyfrowych i kart kierowcy",
+		content:
+			"Wszystkie samochody o DMC >3,5t zarejestrowane od 1 maja 2006 wyposażone zostały w cyfrowe urządzenia rejestrujące. Jest to bardzo wygodne rozwiązanie dla kierowców wykonujących przewozy drogowe. Używając cyfrowych kart kierowcy nie muszą się już martwić o poprawność danych wpisywanych na wykresówkach używanych do rejestrowania aktywności na tradycyjnych tachografach analogowych. Przedsiębiorca ma obowiązek sczytywania karty kierowcy minimum co 28 dni oraz tachografu minimum co 90 dni. Współpracując z nami macie Państwo zagwarantowane terminowe odczyty bezpośrednio w siedzibie firmy lub w dogodnym miejscu dla Państwa.",
+	},
+	{
+		id: 7,
+		title: "Regulamin pracy kierowców",
+		content:
+			"Tworzymy regulaminy pracy kierowców. Dzięki nim wszystko jest łatwiejsze. W przypadku sytuacji konfliktowej lub niejasnej, prawidłowo napisany regulamin pracy potrafi rozwiać wątpliwości i jasno określić obowiązki każdej ze stron. Pozwala też w dużej mierze zaoszczędzić zarobione pieniądze firmy.",
+	},
+	{
+		id: 8,
+		title: "Szkolenia kierowców",
+		content:
+			"90 % błędów podczas jazdy spowodowanych jest niewiedzą kierowcy. Są to błędy bardzo kłopotliwe i kosztowne, ponieważ popełniane są regularnie. Po tym szkoleniu każdy kierowca dwa razy przemyśli wszystkie swoje decyzje zanim popełni jakiekolwiek naruszenie. Dzięki szczegółowej analizie będziemy w stanie pokazać kierowcy jego pomyłki, uświadamiając go, jak powinien pracować, aby uniknąć kar podczas kontroli. Szkoląc swój zespół kierowców w dużej mierze przyczyniamy się do tego, aby w przyszłości dotkliwe kary nas ominęły. Rutynowe szkolenia są przeprowadzane z wszystkimi kierowcami na bieżąco po przeprowadzonej analizie. Na Państwa życzenie nasz doświadczony doradca przeprowadzi szkolenie z przestrzegania norm czasu pracy, oraz z prawidłowej obsługi tachografów analogowych i tachografów cyfrowych.",
+	},
+	{
+		id: 9,
+		title: "Zarządzanie środkami transportu",
+		content:
+			"Załatwianie męczących formalności, jak również wypełnianie oraz składanie formularzy w odpowiednich urzędach przestanie być problemem. Oferujemy bowiem pomoc również w tym zakresie. Pomożemy Ci w uzyskaniu licencji na prowadzenie firmy transportowej, jak również we wszelkich innych formalnościach. Działając na podstawie ustawy o transporcie drogowym oferujemy nawiązanie współpracy z podmiotami nie posiadającymi certyfikatu kompetencji zawodowych przewoźnika. Podpisanie umowy z naszą firmą, zwalnia podmioty chcące starać się o uzyskanie licencji transportowej z obowiązku posiadania certyfikatu kompetencji zawodowych.",
+	},
+	{
+		id: 10,
+		title: "Inne usługi",
+		content:
+			"Pragniemy kompleksowo obsługiwać wszystkie współpracujące z nami firmy i dlatego jesteśmy w stanie dostosować naszą elastyczna ofertę do Państwa wymagań.",
+	},
+];
---
+++ src/global-styles/variables.js
@@ -0,0 +1,32 @@
+export const sizes = {
+	sizeXXS: `14px`,
+	sizeXS: `16px`,
+	sizeS: `18px`,
+	sizeM: `20px`,
+	sizeL: `22px`,
+	sizeXL: `24px`,
+	sizeXXL: `28px`,
+	sizeXXXL: `36px`,
+};
+
+export const colors = {
+	black: "rgb(20, 20, 20)",
+	greyDarness: "rgb(47, 42, 42)",
+	greyDark: "#383e40",
+	greyLight: "#f8f8f8",
+	blueNormal: `rgb(23, 23, 184)`,
+};
+
+export const mediaBreakpoints = {
+	xSmall: `480px`,
+	small: `640px`,
+	medium: `800px`,
+	large: `1040px`,
+	xLarge: `1280px`,
+};
+
+export const additional = {
+	radius: "5px",
+	bigRadius: "20px",
+	transition: "3s",
+};
