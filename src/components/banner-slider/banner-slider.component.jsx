a66be4ff73d154e804d7acaa54d98cd43396edc9
DawidWebb <tomasz.matras@developerweb.pl>
2023-02-07 21:43:44 +0100

    Add change logo for nav logo and banner button

--- src/components/banner-slider/banner-slider.component.jsx
+++ src/components/banner-slider/banner-slider.component.jsx
@@ -1,97 +1,122 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { ReservationContext } from "../../contexts/reservation-view.context";
 
 import styles from "./banner-slider.module.scss";
 
 const BannerSlider = () => {
-	const BANNER_SLIDER = [
-		{
-			id: 0,
-			content: "Ewidencja czasu pracy kierowców",
-			imgPathSmall: "images/banner/digital-print-1198858_640.jpg",
-			imgPathBig: "images/banner/digital-print-1198858_1280.jpg",
-			urlPath: "ewidencja-czasu-pracy-kierowcy",
-		},
-		{
-			id: 1,
-			content: "Prawo, szkolenia, zarządzanie",
-			imgPathSmall: "images/banner/regulation-3246979_640.jpg",
-			imgPathBig: "images/banner/regulation-3246979_1280.jpg",
-			urlPath: "/szkolenia-dla-kierowcow",
-		},
-		{
-			id: 2,
-			content: "Wypożyczalnia",
-			imgPathSmall: "images/banner/car-1376083_640.jpg",
-			imgPathBig: "images/banner/car-1376083_1280.jpg",
-			urlPath: "/",
-		},
+  const BANNER_SLIDER = [
+    {
+      id: 0,
+      content: "Ewidencja czasu pracy kierowców",
+      imgPathSmall: "images/banner/digital-print-1198858_640.jpg",
+      imgPathBig: "images/banner/digital-print-1198858_1280.jpg",
+      urlPath: "ewidencja-czasu-pracy-kierowcy",
+    },
+    {
+      id: 1,
+      content: "Prawo, szkolenia, zarządzanie",
+      imgPathSmall: "images/banner/regulation-3246979_640.jpg",
+      imgPathBig: "images/banner/regulation-3246979_1280.jpg",
+      urlPath: "/szkolenia-dla-kierowcow",
+    },
+    {
+      id: 2,
+      content: "Wypożyczalnia",
+      imgPathSmall: "images/banner/car-1376083_640.jpg",
+      imgPathBig: "images/banner/car-1376083_1280.jpg",
+      urlPath: "/reservation",
+    },
+  ];
 
-	];
-	const [picIndex, setPicIndex] = useState(0);
+  const { isReasrvartionPage, setIsReasrvartionPage } =
+    useContext(ReservationContext);
 
-	let indexNo = picIndex;
-	const changeIndex = () => {
-		indexNo++;
-		if (indexNo >= BANNER_SLIDER.length) {
-			indexNo = 0;
-		}
-		setPicIndex(indexNo);
-	};
-	useEffect(() => {
-		const interval = setInterval(() => {
-			changeIndex();
-		}, 7000);
+  const [picIndex, setPicIndex] = useState(0);
 
-		return () => {
-			clearInterval(interval);
-		};
-	});
+  let indexNo = picIndex;
+  const changeIndex = () => {
+    indexNo++;
+    if (indexNo >= BANNER_SLIDER.length) {
+      indexNo = 0;
+    }
+    setPicIndex(indexNo);
+  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      changeIndex();
+    }, 7000);
 
-	const slider = BANNER_SLIDER.map((item) => (
-		<div key={ item.id } className={ styles.image } style={ { backgroundImage: `url(${ item.imgPathBig })`, left: "0%" } }>
-			<div className={ styles.content } style={ {} }>
-				<p>{ item.content } </p>
-			</div>
+    return () => {
+      clearInterval(interval);
+    };
+  });
 
-		</div>
-	));
+  const handleSwithLogo = (e) => {
+    if (e.target.id === "/reservation") {
+      setIsReasrvartionPage(true);
+    } else {
+      setIsReasrvartionPage(false);
+    }
+  };
 
-	const slideShow = () => {
-		slider[picIndex].props.style.opacity = "1";
-		// slider[picIndex === BANNER_SLIDER.length ? 0 : picIndex].props.style.opacity = "0";
-		slider[picIndex].props.children.props.style.opacity = "1";
-		// slider[picIndex === BANNER_SLIDER.length ? 0 : picIndex].props.children.props.style.opacity = "1";
-		slider[picIndex].props.children.props.style.top = "20%";
-		// slider[picIndex === BANNER_SLIDER.length ? 0 : picIndex].props.children.props.style.top = "40%";
-	};
-	slideShow();
+  const slider = BANNER_SLIDER.map((item) => (
+    <div
+      key={item.id}
+      className={styles.image}
+      style={{ backgroundImage: `url(${item.imgPathBig})`, left: "0%" }}
+    >
+      <div className={styles.content} style={{}}>
+        <p>{item.content} </p>
+      </div>
+    </div>
+  ));
 
-	const handleChangeSlide = (e) => {
-		setPicIndex(e.target.id);
-	};
+  const slideShow = () => {
+    slider[picIndex].props.style.opacity = "1";
+    // slider[picIndex === BANNER_SLIDER.length ? 0 : picIndex].props.style.opacity = "0";
+    slider[picIndex].props.children.props.style.opacity = "1";
+    // slider[picIndex === BANNER_SLIDER.length ? 0 : picIndex].props.children.props.style.opacity = "1";
+    slider[picIndex].props.children.props.style.top = "20%";
+    // slider[picIndex === BANNER_SLIDER.length ? 0 : picIndex].props.children.props.style.top = "40%";
+  };
+  slideShow();
 
-	const dots = BANNER_SLIDER.map((item) => (
-		<div key={ item.id } id={ item.id } className={ styles.dot } style={ {} } onClick={ handleChangeSlide }></div>
-	));
+  const handleChangeSlide = (e) => {
+    setPicIndex(e.target.id);
+  };
 
+  const dots = BANNER_SLIDER.map((item) => (
+    <div
+      key={item.id}
+      id={item.id}
+      className={styles.dot}
+      style={{}}
+      onClick={handleChangeSlide}
+    ></div>
+  ));
 
+  const activeDot = () => {
+    dots[picIndex].props.style.backgroundColor = "rgb(180, 180, 180)";
+    dots[picIndex].props.style.border = "2px solid rgb(23, 23, 184)";
+  };
+  activeDot();
 
-	const activeDot = () => {
-		dots[picIndex].props.style.backgroundColor = "rgb(180, 180, 180)";
-		dots[picIndex].props.style.border = "2px solid rgb(23, 23, 184)";
-	};
-	activeDot();
-
-	return (
-		<div className={ styles.wrapper }>
-			{ slider }
-			<div className={ styles.links }>
-				<Link to={ `${ BANNER_SLIDER[picIndex].urlPath }` }>Zobacz</Link>
-			</div>
-			{/* <div className={ styles.dots }>{ dots }</div> */ }
-		</div>
-	);
+  return (
+    <div className={styles.wrapper}>
+      {slider}
+      <div className={styles.links}>
+        <Link
+          id={BANNER_SLIDER[picIndex].urlPath}
+          onClick={handleSwithLogo}
+          to={`${BANNER_SLIDER[picIndex].urlPath}`}
+        >
+          Zobacz
+        </Link>
+      </div>
+      {/* <div className={ styles.dots }>{ dots }</div> */}
+    </div>
+  );
 };
 
 export default BannerSlider;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,62 +1,107 @@
-import { useContext, Fragment, useState } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { useContext, Fragment, useState } from "react";
+import { Outlet, Link } from "react-router-dom";
 import { DropDownMenu } from "../../routes";
 import { ReservationContext } from "../../contexts/reservation-view.context";
 import styles from "./navigation.module.scss";
-import { logDOM } from '@testing-library/react';
+import { logDOM } from "@testing-library/react";
 
 const Navigation = () => {
+  const { isReasrvartionPage, setIsReasrvartionPage } =
+    useContext(ReservationContext);
 
-    const {isReasrvartionPage, setIsReasrvartionPage} = useContext(ReservationContext)
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleOpenCloseMenu = (e) => {
+    setIsMenuOpen(!isMenuOpen);
+    if (e.target.id === "reservation") {
+      setIsReasrvartionPage(true);
+    } else {
+      setIsReasrvartionPage(false);
+    }
+  };
 
-    const handleOpenCloseMenu = (e) => {
-        setIsMenuOpen(!isMenuOpen);
-       if(e.target.id === "reservation"){
-        setIsReasrvartionPage(true)
-       } else {
-        setIsReasrvartionPage(false)
-       }
-    };
+  const handleSwitchLogo = () => {
+    setIsReasrvartionPage(false);
+  };
 
-    console.log(isReasrvartionPage);
-
-
-    return (
-        <Fragment>
-            <div className={ styles.navWrapper }>
-                <Link to="/" className={styles.navLink}>
-                    <div className={ styles.navLogo }>
-                      { !isReasrvartionPage ? ( <img src="images/logo_maw.png" alt="logo" />) : ( <img src="images/logo_wyp.png" alt="logo-wypozyczalnia" />)}
-                    </div>
-                </Link>
-                <div className={ styles.navBurger } onClick={ handleOpenCloseMenu }>
-                    { !isMenuOpen ? (
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none" /><path d="M3 18h13v-2H3v2zm0-5h10v-2H3v2zm0-7v2h13V6H3zm18 9.59L17.42 12 21 8.41 19.59 7l-5 5 5 5L21 15.59z" /></svg>
-                    ) :
-                        (
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none" /><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" /></svg>
-                        )
-                    }
-
-
-                </div>
-                <div className={ !isMenuOpen ? styles.navLinksClose : styles.navLinksOpen }>
-                    <Link id="start" to="/" onClick={ handleOpenCloseMenu }>Home</Link>
-                    <Link id="offer" to="/offer" onClick={ handleOpenCloseMenu }>Oferta</Link>
-                    {/* <DropDownMenu /> */ }
-                    <Link id="certificates" to="/certificates" onClick={ handleOpenCloseMenu }>Certyfikaty</Link>
-                    <Link id="benefits" to="/benefits" onClick={ handleOpenCloseMenu }>Korzyści</Link>
-                    <Link id="law" to="/law" onClick={ handleOpenCloseMenu }>Prawo</Link>
-                    <Link id="blog" to="/blog" onClick={ handleOpenCloseMenu }>Blog</Link>
-                    <Link id="contact" to="/contact" onClick={ handleOpenCloseMenu }>Kontakt</Link>
-                    <Link id="reservation" to="/reservation" onClick={ handleOpenCloseMenu }>Wypożyczalnia</Link>
-                </div>
-            </div>
-            <Outlet />
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <div className={styles.navWrapper}>
+        <Link to="/" className={styles.navLink} onClick={handleSwitchLogo}>
+          <div className={styles.navLogo}>
+            {!isReasrvartionPage ? (
+              <img src="images/logo_maw.png" alt="logo" />
+            ) : (
+              <img src="images/logo_wyp.png" alt="logo-wypozyczalnia" />
+            )}
+          </div>
+        </Link>
+        <div className={styles.navBurger} onClick={handleOpenCloseMenu}>
+          {!isMenuOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              fill="#000000"
+            >
+              <path d="M0 0h24v24H0V0z" fill="none" />
+              <path d="M3 18h13v-2H3v2zm0-5h10v-2H3v2zm0-7v2h13V6H3zm18 9.59L17.42 12 21 8.41 19.59 7l-5 5 5 5L21 15.59z" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              fill="#000000"
+            >
+              <path d="M0 0h24v24H0z" fill="none" />
+              <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+            </svg>
+          )}
+        </div>
+        <div
+          className={!isMenuOpen ? styles.navLinksClose : styles.navLinksOpen}
+        >
+          <Link id="start" to="/" onClick={handleOpenCloseMenu}>
+            Home
+          </Link>
+          <Link id="offer" to="/offer" onClick={handleOpenCloseMenu}>
+            Oferta
+          </Link>
+          {/* <DropDownMenu /> */}
+          <Link
+            id="certificates"
+            to="/certificates"
+            onClick={handleOpenCloseMenu}
+          >
+            Certyfikaty
+          </Link>
+          <Link id="benefits" to="/benefits" onClick={handleOpenCloseMenu}>
+            Korzyści
+          </Link>
+          <Link id="law" to="/law" onClick={handleOpenCloseMenu}>
+            Prawo
+          </Link>
+          <Link id="blog" to="/blog" onClick={handleOpenCloseMenu}>
+            Blog
+          </Link>
+          <Link id="contact" to="/contact" onClick={handleOpenCloseMenu}>
+            Kontakt
+          </Link>
+          <Link
+            id="reservation"
+            to="/reservation"
+            onClick={handleOpenCloseMenu}
+          >
+            Wypożyczalnia
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
 };
 
 export default Navigation;
