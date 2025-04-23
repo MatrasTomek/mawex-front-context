e80a8413bc4bfac4a665abefab6f02cd8506ba72
Tomasz Matras <matras.tomasz@outlook.com>
2025-04-23 20:00:46 +0200

    Add phones and google info to tacho and account pages

--- src/routes/account-page/account-page.compoent.jsx
+++ src/routes/account-page/account-page.compoent.jsx
@@ -20,12 +20,28 @@ const AccountPage = () => {
 			</div>
 			<div className={styles.infoConetnt}>
 				<h1>Dlaczego warto nam zaufać?</h1>
-				<div className={styles.siteContent}>{accountItems}</div>
-				<div className={styles.linkButtonWrapper}>
-					<a className={styles.linkButton} href="/contact">
-						Skontaktuj się już dziś!
-					</a>
+				<div className={styles.contactInfoWrapper}>
+					<span className={styles.contactPhone}>
+						Skontaktuj się już dziś:
+						<a href={`tel:${'+48 793 272 000'}`}>
+							<span>{'+48 793 272 000'}</span>
+						</a>
+					</span>
+					<div className={styles.linkButtonWrapper}>
+						<a className={styles.linkButton} href="/contact">
+							Lub skorzystaj z formularza
+						</a>
+					</div>
 				</div>
+				<div className={styles.siteContent}>{accountItems}</div>
+			</div>
+			<div className={styles.offerInfoWrapper}>
+				<h3>
+					Zapraszamy do zapoznania sią z Naszą ofertą na{' '}
+					<a className={styles.aButton} target="blank" href="https://g.co/kgs/p2LDJ1P">
+						Google
+					</a>
+				</h3>
 			</div>
 		</div>
 	);
--- src/routes/tacho-service-page/tacho-service-page.compoent.jsx
+++ src/routes/tacho-service-page/tacho-service-page.compoent.jsx
@@ -17,6 +17,23 @@ const TachoServicePage = () => {
 			<div className={styles.infoConetnt}>
 				<h1>Profesjonalna obsługa tachografów – szybko, sprawnie, zgodnie z przepisami</h1>
 				<div className={styles.imageTop}></div>
+
+				<div className={styles.contactInfoWrapper}>
+					<span className={styles.contactPhone}>
+						<p> Skontaktuj się już dziś:</p>
+						<a href={`tel:${'+48 793 272 000'}`}>
+							<span>{'+48 793 272 000'}</span>
+						</a>
+						<a href={`tel:${'+48 500 814 312'}`}>
+							<span>{'+48 500 814 312'}</span>
+						</a>
+					</span>
+					<div className={styles.linkButtonWrapper}>
+						<a className={styles.linkButton} href="/contact">
+							Lub skorzystaj z formularza
+						</a>
+					</div>
+				</div>
 				<h2>
 					W Mawex oferujemy kompleksowy serwis tachografów, obejmujący zarówno urządzenia analogowe, jak i cyfrowe. Dzięki
 					wieloletniemu doświadczeniu i nowoczesnemu sprzętowi gwarantujemy najwyższą jakość usług oraz zgodność z obowiązującymi
@@ -41,10 +58,15 @@ const TachoServicePage = () => {
 						Nie ryzykuj mandatów i problemów z kontrolą drogową – zaufaj ekspertom! Skontaktuj się z nami i umów na serwis
 						tachografu.
 					</h3>
-					<a className={styles.linkButton} href="/contact">
-						Skontaktuj się już dziś!
-					</a>
 				</div>
+			</div>
+			<div className={styles.offerInfoWrapper}>
+				<h3>
+					Zapraszamy do zapoznania sią z Naszą ofertą na{' '}
+					<a className={styles.aButton} target="blank" href="https://g.co/kgs/3RKW2z7">
+						Google
+					</a>
+				</h3>
 			</div>
 		</div>
 	);
